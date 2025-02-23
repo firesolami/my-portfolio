464db9585da4be632f3611da6c2e05fb7f9bfe63
Oluwole <oluwoleafolabi0@gmail.com>
2025-02-23 13:10:21 +0100

    feat: add shrek animation

--- script.js
+++ script.js
@@ -10,3 +10,37 @@ document.addEventListener("DOMContentLoaded", function() {
   const currentYear = new Date().getFullYear();
   copyrightElement.innerHTML = `Copyright &#169; ${currentYear} Oluwole Afolabi. All Rights Reserved.`;
 });
+
+// Function to handle GIF animation
+function handleThoughtBubbleAnimation() {
+  const thoughtBubble = document.querySelector('.thought-bubble');
+  const gif = thoughtBubble.querySelector('img');
+  
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        thoughtBubble.classList.add('visible');
+        gif.classList.add('playing');
+        
+        // Load the PNG image
+        const staticImage = new Image();
+        staticImage.src = './assets/shrek-meme.png';
+        
+        // Load the GIF to get its duration
+        const tempGif = new Image();
+        tempGif.src = gif.src;
+        
+        tempGif.onload = () => {
+          setTimeout(() => {
+            gif.src = './assets/shrek-meme.png'; // Switch to PNG
+            observer.disconnect();
+          }, 6560);
+        };
+      }
+    });
+  }, { threshold: 0.5 });
+
+  observer.observe(thoughtBubble);
+}
+
+document.addEventListener('DOMContentLoaded', handleThoughtBubbleAnimation);
